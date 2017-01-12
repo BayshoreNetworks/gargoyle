2b77a0d490d701dc314550ec45d8310d4b2eb1e2
Dre <andres@neurofuzzsecurity.com>
2017-01-12 13:22:47 -0500

    built in some safeguards related to the data coming backk from the DB

--- main_iptables_monitor.cpp
+++ main_iptables_monitor.cpp
@@ -58,6 +58,7 @@ volatile sig_atomic_t stop;
 void handle_signal (int signum) {
 	stop = 1;
 	syslog(LOG_INFO | LOG_LOCAL6, "%s", "signal caught, program terminating");
+	exit(0);
 }
 
 
@@ -89,78 +90,91 @@ void run_monitor() {
 	char *host_ip = (char*) malloc(dst_buf_sz1 + 1);
 
 	resp3 = get_detected_hosts_all_active_unprocessed(l_hosts3, dst_buf_sz);
-	/*
-	 std::cout << std::endl << resp3 << std::endl;
-	 std::cout << l_hosts3 << std::endl;
-	 */ 
-	token1 = strtok_r(l_hosts3, tok1, &token1_save);
-	while (token1 != NULL) {
-		iter_cnt = 0;
-		//std::cout << token1 << std::endl;
-		token2 = strtok_r(token1, tok2, &token2_save);
-		while (token2 != NULL) {
-
-			//std::cout << token2 << " - " << iter_cnt << std::endl;
-			if (iter_cnt == 0) {
-				row_ix = atoi(token2);
-			} else if (iter_cnt == 1) {
-				host_ix = atoi(token2);
-			} else if (iter_cnt == 2) {
-				timestamp = atoi(token2);
-			} else if (iter_cnt == 3) {
-				active = atoi(token2);
-			} else if (iter_cnt == 4) {
-				processed = atoi(token2);
-			}
-
-			iter_cnt++;
-			token2 = strtok_r(NULL, tok2, &token2_save);
-
-		}
-
-		//std::cout << row_ix << " - " << host_ix << " - " << timestamp << " - " << active << " - " << processed << std::endl;
-		now = (int) time(NULL);
-		*host_ip = 0;
-		rule_ix = 0;
+	if (resp3 == 0) {
 		/*
-		get_host_by_ix(host_ix, host_ip);
-		std::cout << host_ip << std::endl;
-		 */
-		// has it been in jail long enuf?
-		if ((now - timestamp) >= LOCKOUT_TIME) {
-
-			// we have the host ix so get the ip addr from the DB
-			get_host_by_ix(host_ix, host_ip, dst_buf_sz1);
-			//std::cout << host_ip << std::endl;
-
-			// we have the ip addr so get the rule ix from iptables
-			rule_ix = iptables_find_rule_in_chain(MONITOR_CHAIN_NAME, host_ip);
-			if (rule_ix > 0 && host_ip) {
-
-				tstamp = 0;
-				// delete rule from chain
+		 std::cout << std::endl << resp3 << std::endl;
+		 std::cout << l_hosts3 << std::endl;
+		 */ 
+		token1 = strtok_r(l_hosts3, tok1, &token1_save);
+		while (token1 != NULL) {
+			
+			iter_cnt = 0;
+			timestamp = 0;
+			host_ix = 0;
+			active = -1;
+			processed = -1;
+			row_ix = -1;
+			
+			//std::cout << token1 << std::endl;
+			token2 = strtok_r(token1, tok2, &token2_save);
+			while (token2 != NULL) {
+	
+				//std::cout << token2 << " - " << iter_cnt << std::endl;
+				if (iter_cnt == 0) {
+					row_ix = atoi(token2);
+				} else if (iter_cnt == 1) {
+					host_ix = atoi(token2);
+				} else if (iter_cnt == 2) {
+					timestamp = atoi(token2);
+				} else if (iter_cnt == 3) {
+					active = atoi(token2);
+				} else if (iter_cnt == 4) {
+					processed = atoi(token2);
+				}
+	
+				iter_cnt++;
+				token2 = strtok_r(NULL, tok2, &token2_save);
+			}
+			
+			if (row_ix >= 0 && active >= 0 && processed >= 0 && host_ix > 0) {
+	
+				//std::cout << row_ix << " - " << host_ix << " - " << timestamp << " - " << active << " - " << processed << std::endl;
+				now = (int) time(NULL);
+				*host_ip = 0;
+				rule_ix = 0;
 				/*
-				char str[15];
-				snprintf (str, 15, "%d", rule_ix);
-				iptables_delete_rule_from_chain(MONITOR_CHAIN_NAME, str);
+				get_host_by_ix(host_ix, host_ip);
+				std::cout << host_ip << std::endl;
 				 */
-				iptables_delete_rule_from_chain(MONITOR_CHAIN_NAME, rule_ix);
-
-				tstamp = (int) time(NULL);			
-				syslog(LOG_INFO | LOG_LOCAL6, "%s-%s=\"%s\" %s=\"%d\"", "unblocked", MONITOR_VIOLATOR_SYSLOG, host_ip, MONITOR_TIMESTAMP_SYSLOG, tstamp);
-
-				/*
-				 * update DB set active=0, processed=1
-				 * 
-				 * do this even if there is no rule in iptables
-				 * because someone else could have deleted it or
-				 * flushed the rules. if the time threshold is passed
-				 * the DB table must get updated
-				 */
-				modify_host_set_processed_ix(row_ix);
+				// has it been in jail long enuf?
+				if ((now - timestamp) >= LOCKOUT_TIME) {
+		
+					// we have the host ix so get the ip addr from the DB
+					get_host_by_ix(host_ix, host_ip, dst_buf_sz1);
+					//std::cout << "HOST IP: " << host_ip << std::endl;
+		
+					// we have the ip addr so get the rule ix from iptables
+					rule_ix = iptables_find_rule_in_chain(MONITOR_CHAIN_NAME, host_ip);
+					//std::cout << "RULE IX: " << rule_ix << std::endl;
+					if (rule_ix > 0 && host_ip) {
+		
+						tstamp = 0;
+						// delete rule from chain
+						/*
+						char str[15];
+						snprintf (str, 15, "%d", rule_ix);
+						iptables_delete_rule_from_chain(MONITOR_CHAIN_NAME, str);
+						 */
+						iptables_delete_rule_from_chain(MONITOR_CHAIN_NAME, rule_ix);
+		
+						tstamp = (int) time(NULL);			
+						syslog(LOG_INFO | LOG_LOCAL6, "%s-%s=\"%s\" %s=\"%d\"", "unblocked", MONITOR_VIOLATOR_SYSLOG, host_ip, MONITOR_TIMESTAMP_SYSLOG, tstamp);
+		
+						/*
+						 * update DB set active=0, processed=1
+						 * 
+						 * do this even if there is no rule in iptables
+						 * because someone else could have deleted it or
+						 * flushed the rules. if the time threshold is passed
+						 * the DB table must get updated
+						 */
+						modify_host_set_processed_ix(row_ix);
+					}
+					//std::cout << std::endl;
+				}
+				token1 = strtok_r(NULL, tok1, &token1_save);
 			}
 		}
-		token1 = strtok_r(NULL, tok1, &token1_save);
 	}
 	free(l_hosts3);
 	free(host_ip);
