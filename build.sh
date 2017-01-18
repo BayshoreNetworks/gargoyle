48e8e5ce8771549cf91e0e262ab989a21196b905
Beau Taub <beau.taub@bayshorenetworks.com>
2017-01-18 10:29:41 -0500

    automate db and config copy

--- build.sh
+++ build.sh
@@ -3,4 +3,16 @@
 MYPREFIX=/opt/gargoyle_pscand
 ./autogen.sh
 ./configure --prefix=$MYPREFIX --bindir=$MYPREFIX
+
+mkdir -p $MYPREFIX/db/
+
+if [ ! -f $MYPREFIX/db/port_scan_detect.db ]; then
+   cp db/port_scan_detect.db $MYPREFIX/db/
+fi
+
+if [ ! -f $MYPREFIX/.gargoyle_config ]; then
+   cp .gargoyle_config $MYPREFIX
+fi
+
 make clean
+make
