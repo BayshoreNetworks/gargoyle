3b4d5971ea5bb3dec72ae492c8321a25ed18f918
Dre <andres@neurofuzzsecurity.com>
2017-01-20 17:58:30 -0500

    added API call to add rows to the hosts_ports table upon detection of NULL/FIN/XMAS scans

--- packet_handler.cpp
+++ packet_handler.cpp
@@ -940,6 +940,10 @@ int GargoylePscandHandler::xmas_scan(
 
 			if (ADD_RULES_KNOWN_SCAN_AGGRESSIVE) {
 				add_block_rule(src_ip, 3);
+				
+				int host_ix = add_ip_to_hosts_table(src_ip);
+				if (host_ix > 0)
+					add_to_hosts_port_table(host_ix, dst_port, 1);
 			}
 
 			add_to_scanned_ports_dict(dst_ip, src_port);
@@ -970,6 +974,10 @@ int GargoylePscandHandler::fin_scan(
 
 				if (ADD_RULES_KNOWN_SCAN_AGGRESSIVE) {
 					add_block_rule(src_ip, 2);
+					
+					int host_ix = add_ip_to_hosts_table(src_ip);
+					if (host_ix > 0)
+						add_to_hosts_port_table(host_ix, dst_port, 1);
 				}
 
 				add_to_scanned_ports_dict(dst_ip, src_port);
@@ -999,10 +1007,14 @@ int GargoylePscandHandler::null_scan(
 
 			if (ADD_RULES_KNOWN_SCAN_AGGRESSIVE) {
 				add_block_rule(src_ip, 1);
+				
+				int host_ix = add_ip_to_hosts_table(src_ip);
+				if (host_ix > 0)
+					add_to_hosts_port_table(host_ix, dst_port, 1);
 			}
 
 			add_to_scanned_ports_dict(dst_ip, src_port);
-
+			
 			if (is_in_black_listed_hosts(src_ip) == false) {
 				BLACK_LISTED_HOSTS.insert(src_ip);
 			}
@@ -1087,11 +1099,9 @@ void GargoylePscandHandler::add_block_rules() {
 
 	int resp;
 	size_t dst_buf_sz = SMALL_DEST_BUF;
-	char *l_hosts;
-	l_hosts = (char*) malloc(dst_buf_sz+1);
+	char *l_hosts = (char*) malloc(dst_buf_sz+1);
 	size_t dst_buf_sz1 = LOCAL_BUF_SZ;
-	char *host_ip;
-	host_ip = (char*) malloc(dst_buf_sz1+1);
+	char *host_ip = (char*) malloc(dst_buf_sz1+1);
 
 	int added_host_ix;
 	added_host_ix = 0;
