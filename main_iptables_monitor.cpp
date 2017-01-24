98de399f9e21a4705e18682a547fb999400df131
Dre <andres@neurofuzzsecurity.com>
2017-01-24 00:43:08 -0500

    added some more protection to the monitor process

--- main_iptables_monitor.cpp
+++ main_iptables_monitor.cpp
@@ -139,32 +139,32 @@ void run_monitor() {
 				if ((now - timestamp) >= LOCKOUT_TIME) {
 		
 					// we have the host ix so get the ip addr from the DB
-					get_host_by_ix(host_ix, host_ip, dst_buf_sz1);
-					//std::cout << "HOST IP: " << host_ip << std::endl;
-		
-					// we have the ip addr so get the rule ix from iptables
-					rule_ix = iptables_find_rule_in_chain(GARGOYLE_CHAIN_NAME, host_ip);
-					//std::cout << "RULE IX: " << rule_ix << std::endl;
-					if (rule_ix > 0 && host_ip) {
-		
-						tstamp = 0;
-						/*
-						 * update DB set active=0, processed=1
-						 * 
-						 * do this even if there is no rule in iptables
-						 * because someone else could have deleted it or
-						 * flushed the rules. if the time threshold is passed
-						 * the DB table must get updated
-						 */
-						if (modify_host_set_processed_ix(row_ix) == 0) {
-							// delete rule from chain
-							iptables_delete_rule_from_chain(GARGOYLE_CHAIN_NAME, rule_ix);
-							
-							tstamp = (int) time(NULL);
-							syslog(LOG_INFO | LOG_LOCAL6, "%s-%s=\"%s\" %s=\"%d\"", "unblocked", VIOLATOR_SYSLOG, host_ip, TIMESTAMP_SYSLOG, tstamp);
+					if (get_host_by_ix(host_ix, host_ip, dst_buf_sz1) == 0) {
+						//std::cout << "HOST IP: " << host_ip << std::endl;
+			
+						// we have the ip addr so get the rule ix from iptables
+						rule_ix = iptables_find_rule_in_chain(GARGOYLE_CHAIN_NAME, host_ip);
+						//std::cout << "RULE IX: " << rule_ix << std::endl;
+						if (rule_ix > 0 && strcmp(host_ip, "") != 0) {
+			
+							tstamp = 0;
+							/*
+							 * update DB set active=0, processed=1
+							 * 
+							 * do this even if there is no rule in iptables
+							 * because someone else could have deleted it or
+							 * flushed the rules. if the time threshold is passed
+							 * the DB table must get updated
+							 */
+							if (modify_host_set_processed_ix(row_ix) == 0) {
+								// delete rule from chain
+								iptables_delete_rule_from_chain(GARGOYLE_CHAIN_NAME, rule_ix);
+								
+								tstamp = (int) time(NULL);
+								syslog(LOG_INFO | LOG_LOCAL6, "%s-%s=\"%s\" %s=\"%d\"", "unblocked", VIOLATOR_SYSLOG, host_ip, TIMESTAMP_SYSLOG, tstamp);
+							}
 						}
 					}
-					//std::cout << std::endl;
 				}
 				token1 = strtok_r(NULL, tok1, &token1_save);
 			}
