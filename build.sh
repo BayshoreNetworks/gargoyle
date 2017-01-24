a67c41407e9ef5383d5083a984a05f6a55387577
Mike Dulaney <k.michael.dulaney@gmail.com>
2017-01-23 20:34:48 -0500

    Now accepts variables from environment

--- build.sh
+++ build.sh
@@ -1,20 +1,34 @@
 #!/bin/sh
 
-MYPREFIX=/opt/gargoyle_pscand
+if [ -z "$DESTDIR" ];
+then
+DESTDIR=/
+fi
+
+if [ -z "$DEPLOY_TO" ];
+then
+DEPLOY_TO=/opt/gargoyle_pscand
+fi
+
 ./autogen.sh
-./configure --prefix=$MYPREFIX --bindir=$MYPREFIX
+./configure DESTDIR=$DESTDIR --prefix=$DEPLOY_TO --bindir=$DEPLOY_TO \
+    --build=i686-pc-linux-gnu --host=$CROSS_COMPILE
 
-mkdir -p $MYPREFIX/db/
+mkdir -p $DEPLOY_TO/db/
 
-if [ ! -f $MYPREFIX/db/port_scan_detect.db ]; then
-   cp db/port_scan_detect.db $MYPREFIX/db/
+if [ ! -f $DEPLOY_TO/db/port_scan_detect.db ]; then
+   cp db/port_scan_detect.db $DEPLOY_TO/db/
 fi
 
-if [ ! -f $MYPREFIX/.gargoyle_config ]; then
-   cp .gargoyle_config $MYPREFIX
+if [ ! -f $DEPLOY_TO/.gargoyle_config ]; then
+   cp .gargoyle_config $DEPLOY_TO
 fi
 
-cp etc-init.d-gargoyle /etc/init.d/gargoyle_pscand
+if [ ! -f $DEPLOY_TO/.gargoyle_internal_port_config ]; then
+   cp .gargoyle_internal_port_config $DEPLOY_TO
+fi
+
+sed -e "s,APPDIR,$DEPLOY_TO,g" etc-init.d-gargoyle>/etc/init.d/gargoyle_pscand
 chmod 770 /etc/init.d/gargoyle_pscand
 systemctl enable gargoyle_pscand
 systemctl daemon-reload
