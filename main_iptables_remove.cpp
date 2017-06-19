e01e7c628b9a17737a2d144136febba65d78d159
Andres Andreu <andres@neurofuzzsecurity.com>
2017-06-19 13:54:46 -0400

    added support for blacklist check

--- main_iptables_remove.cpp
+++ main_iptables_remove.cpp
@@ -49,12 +49,13 @@
 #include "gargoyle_config_vals.h"
 #include "config_variables.h"
 #include "ip_addr_controller.h"
-
+#include "shared_config.h"
 
 bool DEBUG = false;
 size_t IPTABLES_SUPPORTS_XLOCK;
 
 char DB_LOCATION[SQL_CMD_MAX+1];
+SharedIpConfig *gargoyle_monitor_blacklist_shm = NULL;
 
 bool validate_ip_addr(std::string ip_addr)
 {
@@ -102,53 +103,67 @@ int main(int argc, char *argv[])
 			strncpy(ip, argv[1], 15);
 			ip[strlen(ip)] = '\0';
 			
-		    if (DEBUG)
-		    	std::cout << "IP addr: " << ip << std::endl;
-			
-			size_t rule_ix = iptables_find_rule_in_chain(GARGOYLE_CHAIN_NAME, ip, IPTABLES_SUPPORTS_XLOCK);
-			
-		    if (DEBUG)
-		    	std::cout << "RuleIX: " << rule_ix << std::endl;
-			
-			if (rule_ix > 0 && strcmp(ip, "") != 0) {
+			/*
+			 * if this ip is blacklisted leave it alone
+			 */
+			if (!is_black_listed(ip, (void *)gargoyle_monitor_blacklist_shm)) {
 				
-				// find the host ix for the ip
-				int host_ix = get_host_ix(ip, DB_LOCATION);
+
+				if (DEBUG)
+					std::cout << "IP addr: " << ip << std::endl;
 				
-				if (host_ix > 0) {
-
-					// find the row ix for this host (in detected_hosts table)
-					size_t row_ix = get_detected_hosts_row_ix_by_host_ix(host_ix, DB_LOCATION);
-					if (row_ix > 0) {
-						
-					    if (DEBUG)
-					    	std::cout << "Host ix: " << host_ix << std::endl;
-						// delete all records for this host_ix from hosts_ports_hits table
-						remove_host_ports_all(host_ix, DB_LOCATION);
-						
-					    if (DEBUG)
-					    	std::cout << "Row ix: " << row_ix << std::endl;
-						// delete row from detected_hosts
-						remove_detected_host(row_ix, DB_LOCATION);
-						
-						int tstamp = (int) time(NULL);
-
-						// add to ignore ip table
-						add_host_to_ignore(host_ix, tstamp, DB_LOCATION);
-						
-						// reset last_seen to 1972
-						update_host_last_seen(host_ix, DB_LOCATION);
-						
-						iptables_delete_rule_from_chain(GARGOYLE_CHAIN_NAME, rule_ix, IPTABLES_SUPPORTS_XLOCK);
-
-						do_unblock_action_output(ip, tstamp);
-						
+				size_t rule_ix = iptables_find_rule_in_chain(GARGOYLE_CHAIN_NAME, ip, IPTABLES_SUPPORTS_XLOCK);
+				
+				if (DEBUG)
+					std::cout << "RuleIX: " << rule_ix << std::endl;
+				
+				if (rule_ix > 0 && strcmp(ip, "") != 0) {
+					
+					// find the host ix for the ip
+					int host_ix = get_host_ix(ip, DB_LOCATION);
+					
+					if (host_ix > 0) {
+	
+						// find the row ix for this host (in detected_hosts table)
+						size_t row_ix = get_detected_hosts_row_ix_by_host_ix(host_ix, DB_LOCATION);
+						if (row_ix > 0) {
+							
+							if (DEBUG)
+								std::cout << "Host ix: " << host_ix << std::endl;
+							// delete all records for this host_ix from hosts_ports_hits table
+							remove_host_ports_all(host_ix, DB_LOCATION);
+							
+							if (DEBUG)
+								std::cout << "Row ix: " << row_ix << std::endl;
+							// delete row from detected_hosts
+							remove_detected_host(row_ix, DB_LOCATION);
+							
+							int tstamp = (int) time(NULL);
+	
+							// add to ignore ip table
+							add_host_to_ignore(host_ix, tstamp, DB_LOCATION);
+							
+							// reset last_seen to 1972
+							update_host_last_seen(host_ix, DB_LOCATION);
+							
+							iptables_delete_rule_from_chain(GARGOYLE_CHAIN_NAME, rule_ix, IPTABLES_SUPPORTS_XLOCK);
+	
+							do_unblock_action_output(ip, tstamp);
+							
+						}
 					}
 				}
-			}
+			
+			}			
 		}
     } else {
     	std::cout << std::endl << "Usage: ./gargoyle_pscand_unblockip ip_addr" << std::endl << std::endl;
     }
+    
+    if(gargoyle_monitor_blacklist_shm) {
+        delete gargoyle_monitor_blacklist_shm;
+        gargoyle_monitor_blacklist_shm;
+    }
+    
 	return 0;
 }
